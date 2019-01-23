e6e6e88bdbc23905b4bcdcc8aed5d880ecc98bd2
Alec Helbling <alechelbling@gatech.edu>
2019-01-23 13:26:32 -0500

    Reverting changes in Person1.java because the branch did not get pushed to Github

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Alec
+ *  @author Bob
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return input.substring(input.length() - 2) + input.substring(0 ,input.length() - 2);
+	  return null;
 	}
 	
 	/**
