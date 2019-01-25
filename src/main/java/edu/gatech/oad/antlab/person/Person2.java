f004ab32594377f0140fe2f98999fb8b7c638bf0
Jared Carbone <32436031+jaredcarbone2@users.noreply.github.com>
2019-01-25 13:09:13 -0500

    changed person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -9,16 +9,16 @@ package edu.gatech.oad.antlab.person;
  * @version 1.1
  */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
-	 	/**
+	/** Holds the persons real name */
+	private String name;
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String pname) {
+		name = pname;
+	}
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -30,18 +30,35 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		//Person 2 put your implementation here
+		int x = 0;
+
+		String word = "";
+		boolean [] inputBools = new boolean[input.length()];
+		for (int i = 0; i < input.length(); i++) {
+			x = (int) (Math.random() * input.length());
+			while (inputBools[x]) {
+				x = (int)(Math.random() * input.length());
+			}
+			System.out.println(x);
+			inputBools[x] = true;
+			word += input.substring(x, x + 1);
+		}
+
+
+		return word;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
+
+
 }
